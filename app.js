56151361b847eea4b301dabbafb31b8d4c3732da
Liudas Bagdonas <liudasbagd@gmail.com>
2020-09-17 14:07:05 +0300

    added constructor functions

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 const pakelis = {
     vnt: 20,
-    parukyti() {
-        if(this.vnt){
-            this.vnt--
+    parukyt() {
+        if (this.vnt) {
+            this.vnt--;
         }
         return this.vnt;
-    }
+    },
 };
-const body = document.querySelector('body');
-const buttonSmoke = document.createElement('button');
-buttonSmoke.innerText = pakelis.vnt;
 
-buttonSmoke.addEventListener('click', () => {
-    buttonSmoke.innerText = pakelis.parukyti();
+
+function Studentas(value){
+    this.name = value;
+    this.pakelis = {...pakelis}
+}
+// let studentOne = new Studentas('Aiste', {...pakelis});
+// let studentTwo = new Studentas('Laura', {...pakelis});
+// let studentThree = new Studentas('Dominyka', {...pakelis});
+
+const studentai = [
+    'Aiste',
+    'Laura',
+    'Dominyka'
+];
+console.log(studentai)
+studentai.forEach((value, index, array) => {
+    array[index] = new Studentas(value)
 });
 
-body.append(buttonSmoke);
+studentai.forEach(studentas => {
+    const button = createButton(studentas);
+    document.body.appendChild(button);
+});
 
-// function getCancer(cigariukai) {
-//     while(cigariukai.vnt) {
-//         console.log(cigariukai.vnt);
-//         cigariukai.parukyti()
-//     }
-// }
-// getCancer(pakelis)
+function createButton(studentas) {
+    const button = document.createElement('button');
+    button.appendChild(document.createTextNode(`${studentas.name}, liko ${studentas.pakelis.vnt}`));
+
+    button.addEventListener('click', () => {
+        studentas.pakelis.parukyt();
+        button.innerHTML = `${studentas.name}, liko ${studentas.pakelis.vnt}`;
+    });
+
+    return button;
+}
